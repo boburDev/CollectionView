2d0db80ae9acfaf24e2f9d13e5ce8ede863792a5
boburDev <negmatov1907@gmail.com>
2021-02-09 23:36:11 +0500

    initial commit

--- CollectionView/AppDelegate.swift
+++ CollectionView/AppDelegate.swift
@@ -15,9 +15,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        setNavigationBar()
         return true
     }
 
+    func setNavigationBar() {
+//        UINavigationBar.appearance().tintColor = UIColor.black
+//        UINavigationBar.appearance().barTintColor = UIColor.white
+//        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor:UIColor.black]
+//        UINavigationBar.appearance().shadowImage = UIImage()
+//        UINavigationBar.appearance().isTranslucent = false
+//        UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .default)
+    }
+    
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
